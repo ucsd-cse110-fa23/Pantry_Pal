e5f31f2b5d1aaf399ae82b9a34a399522dd5acab
PeterNguyen4 <peternguyen644@gmail.com>
2023-11-12 23:35:32 -0800

    Implement save and cancel buttons on new recipe screen

--- src/ChatGPT.java
+++ src/ChatGPT.java
@@ -8,14 +8,46 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.skin.ButtonSkin;
 import javafx.scene.layout.BorderImage;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import java.io.*;
 
+class gptFooter extends HBox{
+    private Button saveButton;
+    private Button cancelButton;
+
+    String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
+
+    gptFooter() {
+        this.setPrefSize(500, 60);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        this.setSpacing(15);
+
+        saveButton = new Button("Save Recipe");
+        saveButton.setStyle(defaultButtonStyle);
+
+        cancelButton = new Button("Cancel");
+        cancelButton.setStyle(defaultButtonStyle);
+
+        this.getChildren().addAll(saveButton, cancelButton);
+        this.setAlignment(Pos.CENTER);
+    }
+
+    public Button getSaveButton() {
+        return saveButton;
+    }
+
+    public Button getCancelButton() {
+        return cancelButton;
+    }
+}
 public class ChatGPT extends BorderPane{
     private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
     private static final String API_KEY = "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";
@@ -25,7 +57,9 @@ public class ChatGPT extends BorderPane{
     private int maxTokens;
 
     private Header header;
+    private gptFooter footer;
     private Button saveButton;
+    private Button cancelButton;
     private Recipe newRecipe;
     public String recipeName;
     private RecipeList recipeList;
@@ -66,22 +100,21 @@ public class ChatGPT extends BorderPane{
         JSONArray choices = responseJson.getJSONArray("choices");
         String generatedText = choices.getJSONObject(0).getString("text");
 
-        
-        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
-
         header = new Header();
+        footer = new gptFooter();
   
         this.setPrefSize(370, 120);
-        this.setPadding(new Insets(5, 0, 5, 5));
-        saveButton = new Button("Save Recipe");
-        saveButton.setStyle(defaultButtonStyle);
-        this.setBottom(saveButton);
+        
+        this.setBottom(footer);
         
         this.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0; -fx-font-weight: bold;");
         Label recipe = new Label();
         recipe.setText(generatedText);
         this.setTop(header);
         this.setCenter(recipe);
+
+        this.saveButton = footer.getSaveButton();
+        this.cancelButton = footer.getCancelButton();
 
         this.recipeName = generatedText.split("\n")[0];
         this.primaryStage = primaryStage;
@@ -108,9 +141,9 @@ public class ChatGPT extends BorderPane{
 
             newRecipe = new Recipe(primaryStage);
             newRecipe.getRecipe().setText(recipeName);
-            // recipeList.getChildren().add(newRecipe);
+            System.out.println("RECIPE____" + recipeName);
+            System.out.println("____LIST" + generatedText);
             recipeList.getChildren().add(newRecipe);
-
 
             FileWriter writer = new FileWriter("recipes.csv");
             // Write new recipe at the top of the csv
@@ -135,5 +168,8 @@ public class ChatGPT extends BorderPane{
         saveButton.setOnAction(e -> {
             saveRecipe(generatedText);
         }); 
+        cancelButton.setOnAction(e -> {
+            primaryStage.setScene(homeScene);
+        });
     }
 }
